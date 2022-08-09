626f32a8396964995a390449d5b150d342a2aa1b
JaHyeon_Gu <jahyungu@naver.com>
2022-08-09 19:16:12 +0900

    kakaomap html & data.xlsx

--- partnermap/partnerapp/models.py
+++ partnermap/partnerapp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+# 총학생회 class
+class Partner(models.Model):
+    id = models.IntegerField(primary_key=True)
+    type = models.CharField(max_length=10)
+    name = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, null=True)
+    content = models.CharField(max_length=300)
+    address = models.CharField(max_length=50, null=True)
+    
+    def __str__(self):
+        return self.name
+
+
+# 인문대학 class
+class PartnerHumanity(models.Model):
+    id = models.IntegerField(primary_key=True)
+    type = models.CharField(max_length=10)
+    name = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, null=True)
+    content = models.CharField(max_length=300)
+    address = models.CharField(max_length=50, null=True)
+    
+    def __str__(self):
+        return self.name
+
+# 경영대학 class
+class PartnerBusiness(models.Model):
+    id = models.IntegerField(primary_key=True)
+    type = models.CharField(max_length=10)
+    name = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, null=True)
+    content = models.CharField(max_length=300)
+    address = models.CharField(max_length=50, null=True)
+    
+    def __str__(self):
+        return self.name
--- partnermap/partnerapp/views.py
+++ partnermap/partnerapp/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from .models import Partner
+from django.conf import settings
 
-# Create your views here.
 def home(request):
-    return render(request, 'base.html')
+
+    context = {
+        "api_key": settings.KAKAO_MAPS_API_KEY
+    }
+
+    partners = Partner.objects.all()
+    return render(request, 'kakao.html', {'partners':partners})
+
+
+
+
+# def home(request):
+#     # Post에 있는 모든 객체들을 가져오는 코드
+#     # posts = Post.objects.all()
+#     # Post의 객체를 filter를 통해 가져오는 코드
+#     posts = Post.objects.filter().order_by('-date')
+#     # 객체들의 목록을 끊어주는 코드
+#     paginator = Paginator(posts, 5)
+#     pagnum = request.GET.get('page')
+#     posts = paginator.get_page(pagnum)
+#     return render(request, 'index.html', {'posts':posts})
