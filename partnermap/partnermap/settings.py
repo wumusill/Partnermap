e8d42405aa1cbdcaad41231eaf44872623040211
JaHyeon_Gu <jahyungu@naver.com>
2022-08-19 22:40:37 +0900

    revise settings.py for release

--- partnermap/partnermap/settings.py
+++ partnermap/partnermap/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".cloudtype.app"]
+CSRF_TRUSTED_ORIGINS = ['https://*.cloudtype.app']
 
 # Application definition
 
